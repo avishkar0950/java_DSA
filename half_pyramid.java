40c4fd104a4315fbb4ada9c6caee2937749e652d
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2025-01-06 20:06:48 +0530

    Add files via upload

---
+++ half_pyramid.java
@@ -0,0 +1,19 @@
+package practice_program;
+import java.io.*;
+import java.util.*;
+public class half_pyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int n=4;
+        for(int i=1;i<=n;i++)
+        {
+        	for(int j=1;j<=i;j++)
+        	{
+        		System.out.print("*");
+        	}
+        	System.out.println();
+        }
+	}
+
+}
